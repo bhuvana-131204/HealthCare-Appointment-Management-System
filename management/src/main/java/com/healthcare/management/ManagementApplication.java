9bc806dc5bfeab464e88a3cdb7bf52c507faa5a4
bhuvana-131204 <2389408@LTIN579566.cts.com>
2025-03-11 11:17:37 +0530

    Removed git statement from Main class

--- management/src/main/java/com/healthcare/management/ManagementApplication.java
+++ management/src/main/java/com/healthcare/management/ManagementApplication.java
@@ -8,7 +8,7 @@ public class ManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementApplication.class, args);
-		System.out.println("Git check");
+		
 	}
 
 } 
