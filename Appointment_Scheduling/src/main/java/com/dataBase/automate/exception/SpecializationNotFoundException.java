95901f99eb3fcd06434efeab575abdeff7eb35d0
bhuvana-131204 <AllaBhuvana.NagaSai@cognizant.com>
2025-04-22 15:06:21 +0530

    Delete Appointment_Scheduling directory

--- Appointment_Scheduling/src/main/java/com/dataBase/automate/exception/SpecializationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dataBase.automate.exception;
-
-public class SpecializationNotFoundException extends RuntimeException {
-	public SpecializationNotFoundException(String msg) {
-		super(msg);
-	}
-
-}
--- Appointment_Scheduling/src/main/java/com/dataBase/automate/model/Status.java
+++
@@ -1,8 +0,0 @@
-package com.dataBase.automate.model;
-
-public enum Status {
-    Booked,
-    Completed,
-    Pending,
-    Cancelled;
-}
